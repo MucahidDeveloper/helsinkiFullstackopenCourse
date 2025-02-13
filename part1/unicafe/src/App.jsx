16f16c6c1b6b698dae47576b00fbcd077816d8fb
MucahidDeveloper <Mucadad221@gmail.com>
2025-02-14 01:25:28 +0300

    solving 1.6: unicafe step 1 exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <div>
+        <button onClick={handleGood}>Good</button>
+        <button onClick={handleNeutral}>Neutral</button>
+        <button onClick={handleBad}>Bad</button>
+      </div>
+      <h1>Statistics</h1>
+      <div>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
