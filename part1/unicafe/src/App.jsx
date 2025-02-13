748abf1fea8fe6d4ff1d91f0e618f8e0e74f1c73
MucahidDeveloper <Mucadad221@gmail.com>
2025-02-14 01:33:08 +0300

    solving 1.8: unicafe step 3 exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,36 +1,35 @@
 import { useState } from 'react';
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total ? (good - bad) / total : 0;
+  const positivePercentage = total ? (good / total) * 100 : 0;
+
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Average Score: {average}</p>
+      <p>Positive Feedback: {positivePercentage}%</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const handleGood = () => setGood(good + 1);
-  const handleNeutral = () => setNeutral(neutral + 1);
-  const handleBad = () => setBad(bad + 1);
-
-  const total = good + neutral + bad;
-  const average = total ? (good - bad) / total : 0;
-  const positivePercentage = total ? (good / total) * 100 : 0;
-  
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <div>
-        <button onClick={handleGood}>Good</button>
-        <button onClick={handleNeutral}>Neutral</button>
-        <button onClick={handleBad}>Bad</button>
-      </div>
+      <h1>Customer Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
       <h1>Statistics</h1>
-      <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-
-        <p>All: {total}</p>
-        <p>Average: {average}</p>
-        <p>Positive: {positivePercentage}%</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
