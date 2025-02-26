1c6327641cc371781aa89160464f3c04ec32405c
MucahidDeveloper <Mucadad221@gmail.com>
2025-02-26 08:47:29 +0300

    solving 2.9: The Phonebook Step 4 exercise - add search functionality

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const personsArray = [
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]
+
+  const [persons, setPersons] = useState(personsArray) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+    setPersons(personsArray.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase())))
+  
   }
 
   const handleNewPerson = (event) => {
@@ -21,7 +32,7 @@ const App = () => {
       return
     }
     const newPerson = { name: newName, number: newNumber }
-    setPersons([...persons, newPerson])
+    setPersons([...personsArray, newPerson])
     setNewName('')
     setNewNumber('')
   }
@@ -29,13 +40,19 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={handleSearchChange} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleNewPerson}>
       <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
+        <br/>
         <div>
           number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
+        <br/>
         <div>
           <button type="submit">add</button>
         </div>
