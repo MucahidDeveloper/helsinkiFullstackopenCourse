ca4c2a09796a02015c46d4080c20859d9feae823
MucahidDeveloper <Mucadad221@gmail.com>
2025-02-26 07:42:03 +0300

    solving 2.1: Course information step 6 exercise

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,56 +1,29 @@
+import Course from './Course';
+
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
-  }
+  };
 
-  const Header = (props) => {
-    return <h1>{props.course.name}</h1>
-  }
+  return <Course course={course} />;
+};
 
-  const Part = (props) => {
-    return <p>{props.part.name} {props.part.exercises}</p>
-  }
-
-  const Content = (props) => {
-    return (
-      <div>
-        {props.course.parts.map((part, index) => (
-          <Part key={index} part={part} />
-        ))}
-      </div>
-    )
-  }
-
-  const Total = (props) => {
-    return (
-      <p>
-        <strong>Number of exercises:</strong>{" "}
-        {props.course.parts.reduce((sum, part) => sum + part.exercises, 0)}
-      </p>
-    )
-  }
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
-
-export default App
+export default App;
