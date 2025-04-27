126eb2aa56dd9670a58dc898c9a662d931beaa7b
MucahidDeveloper <Mucadad221@gmail.com>
2025-04-27 23:15:13 +0300

    add eslint file

--- part5/.eslintrc.cjs
+++ part5/.eslintrc.cjs
@@ -1,21 +1,32 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/prop-types': false
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-}
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/prop-types": "warn", // Show warning for missing prop-types
+    "prettier/prettier": "error", // Enforce prettier formatting
+    "react/jsx-indent": ["error", 2], // Enforce 2 spaces for indentation in JSX
+    "react/jsx-max-props-per-line": ["error", { maximum: 1, when: "always" }], // Enforce a max of 1 prop per line
+    "no-console": "warn", // Show warning for console logs
+    "no-unused-vars": "warn", // Show warning for unused variables
+    semi: ["error", "always"], // Enforce semicolons at the end of statements
+    quotes: ["error", "single"], // Enforce single quotes
+  },
+  settings: {
+    react: {
+      version: "detect", // Automatically detect the React version
+    },
+  },
+};
